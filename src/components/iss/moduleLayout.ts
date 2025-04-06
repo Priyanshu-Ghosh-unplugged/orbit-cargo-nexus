8989a70b65775a4e8a3ecf7a370dab005508a188
PRIYANSHU GHOSH <2006priyanshughosh@gmail.com>
2025-04-06 13:52:18 +0530

    Add files via upload

---
+++ src/components/iss/moduleLayout.ts
@@ -0,0 +1,22 @@
+export const modulePositions: {
+    [moduleName: string]: {
+      x: number;
+      y: number;
+      rotate?: boolean;
+      width?: number;
+      height?: number;
+      fontSize?: number; 
+    };
+  } = {
+    Unity:    { x: 489, y: 160, rotate: true,  width: 35, height: 25, fontSize: 7 },
+    Destiny:  { x: 493, y: 286, rotate: true,  width: 28, height: 25, fontSize: 7 },
+    Harmony:  { x: 490, y: 353, rotate: true,  width: 32, height: 20, fontSize: 5 },
+    Columbus: { x: 484, y: 250, rotate: true,  width: 45, height: 20, fontSize: 6 },
+    Kibo:     { x: 536, y: 247, rotate: true,  width: 28, height: 28, fontSize: 7 },
+  
+    Zarya:    { x: 568, y: 170, width: 28, height: 28, fontSize: 6  },
+    Zvezda:   { x: 523, y: 217, width: 58, height: 20, fontSize: 7 },
+    Rassvet:  { x: 428, y: 217, width: 58, height: 20, fontSize: 7 },
+  };
+  
+  
